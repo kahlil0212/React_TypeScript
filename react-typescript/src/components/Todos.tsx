2d38ec8dda059bb36265e9961dc551a9e651e7ed
Kahlil <kahlil12@hotmail.com>
2025-07-31 22:53:33 -0400

    Implementing context with typescript

--- react-typescript/src/components/Todos.tsx
+++ react-typescript/src/components/Todos.tsx
@@ -1,14 +1,17 @@
-import Todo from "../models/todo";
+import { useContext } from "react";
 import TodoListItem from "./TodoListItem";
 import classes from './Todos.module.css'
+import { TodosContext } from "../store/todos-context";
 
-const Todos: React.FC<{ items: Todo[], onRemoveTodo: (id: string) => void}> = (props) => {
+const Todos: React.FC = () => {
+
+    const todosCtx = useContext(TodosContext)
 
     
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoListItem onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} key={item.id} todoText={item.text} />
+      {todosCtx.items.map((item) => (
+        <TodoListItem onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)} key={item.id} todoText={item.text} />
       ))}
     </ul>
   );
