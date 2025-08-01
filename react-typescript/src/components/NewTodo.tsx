18519a1243547025bb5a1684b9ead8c25ab61147
Kahlil <kahlil12@hotmail.com>
2025-07-31 21:20:23 -0400

    implementing use ref for todo user input text field

--- react-typescript/src/components/NewTodo.tsx
+++ react-typescript/src/components/NewTodo.tsx
@@ -1,14 +1,27 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        //The ! means that we are sure that the value will never be null. 
+        const enteredText = todoTextInputRef.current!.value;
+
+        if(enteredText.trim().length === 0){
+            //throw an error
+            return;
+        }
+        
     }
 
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef}/>
       <button>Add Todo</button>
     </form>
   );
